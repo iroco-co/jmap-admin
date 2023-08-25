a2112eaa7704394e99daa45139867eb05e2a7983
Bruno Thomas <bruno@barreverte.fr>
2023-08-25 19:26:46 +0200

    feat: add DNS configuration page #6

---
+++ src/routes/account/dns/+page.server.ts
@@ -0,0 +1,13 @@
+import type { PageServerLoad } from './$types';
+import { repository } from "$lib/server/db";
+import { getEmailDomain } from "$lib/email";
+import type { VirtualDomain } from "../../../domain";
+import { removePemMarks } from "$lib/crypto";
+
+export const load: PageServerLoad = async ({ locals }) => {
+  const domain = await repository.getDomain(getEmailDomain(locals.email))
+  return {
+    dkim_public_key: domain?.dkim_private_key ? removePemMarks(domain?.dkim_public_key): "",
+    name: domain?.name
+  }
+}
---
+++ src/routes/account/dns/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { _ } from 'svelte-i18n'
+  import type PageData from '@sveltejs/kit'
+  export let data: PageData
+</script>
+
+<section class="account__dns">
+  <h1>{$_('account.dns.title')}</h1>
+  <pre>
+    @                       IN MX     2 smtp
+    @                   600 IN TXT    "v=spf1 mx a:smtp.{data.name} ~all"
+    smtp                    IN MX     5 smtp
+    imap                    IN MX     10 imap
+    imap                    IN A      178.237.111.140
+    smtp                    IN A      178.237.111.140
+    autodiscover            IN A      178.237.111.140
+    _dmarc                  IN TXT    ( "v=DMARC1;p=quarantine;pct=100;sp=reject;aspf=r;" )
+    dkim._domainkey         IN TXT    ( "h=sha256;k=rsa;p={data.dkim_public_key};t=s; )
+  </pre>
+</section>
