8511e38af8b96318e6bf5cdca462ac9d7915fab5
Bruno Thomas <bruno@barreverte.fr>
2023-10-13 09:40:06 +0200

    fix: linter

--- test/routes/account/index.spec.ts
+++ test/routes/account/index.spec.ts
@@ -1,16 +1,16 @@
-import '@testing-library/jest-dom/extend-expect'
-import { render, waitFor } from '@testing-library/svelte'
-import { screen } from '@testing-library/dom'
-import index from '../../../src/routes/account/+page.svelte'
+import '@testing-library/jest-dom/extend-expect';
+import { render, waitFor } from '@testing-library/svelte';
+import { screen } from '@testing-library/dom';
+import index from '../../../src/routes/account/+page.svelte';
 
-import { addMessages, init } from 'svelte-i18n'
+import { addMessages, init } from 'svelte-i18n';
 
-import en from '../../../src/lib/i18n/en.json'
-import { Role } from '../../../src/domain'
-import { DateTime } from 'luxon'
+import en from '../../../src/lib/i18n/en.json';
+import { Role } from '../../../src/domain';
+import { DateTime } from 'luxon';
 
-addMessages('en', en)
-init({ fallbackLocale: 'en', initialLocale: 'en' })
+addMessages('en', en);
+init({ fallbackLocale: 'en', initialLocale: 'en' });
 describe('Account information', () => {
 	test('render page', async () => {
 		render(index, {
@@ -18,14 +18,14 @@ describe('Account information', () => {
 				user: { email: 'foo@bar.com', firstname: 'Foo', lastname: 'Bar', role: Role.User },
 				quota: { id: 'mail', used: 50, total: 200 }
 			}
-		})
-		screen.getByText('Welcome')
+		});
+		screen.getByText('Welcome');
 
-		await waitFor(() => expect(screen.getByText('Foo')).toBeInTheDocument())
-		screen.getByText('Bar')
-		screen.getByText('foo@bar.com')
-		screen.getByText('50 B/200 B (25%)')
-	})
+		await waitFor(() => expect(screen.getByText('Foo')).toBeInTheDocument());
+		screen.getByText('Bar');
+		screen.getByText('foo@bar.com');
+		screen.getByText('50 B/200 B (25%)');
+	});
 
 	test('display number of trial days left', async () => {
 		render(index, {
@@ -39,11 +39,11 @@ describe('Account information', () => {
 				},
 				quota: { id: 'mail', used: 50, total: 200 }
 			}
-		})
+		});
 		await waitFor(() =>
 			expect(screen.queryByText('You have 27 trial days left.')).toBeInTheDocument()
-		)
-	})
+		);
+	});
 
 	test('do not display number of trial days left for regular user', async () => {
 		render(index, {
@@ -57,9 +57,9 @@ describe('Account information', () => {
 				},
 				quota: { id: 'mail', used: 50, total: 200 }
 			}
-		})
-		await waitFor(() => expect(screen.queryByText('You have 27 trial days left.')).toBeNull())
-	})
+		});
+		await waitFor(() => expect(screen.queryByText('You have 27 trial days left.')).toBeNull());
+	});
 
 	test('display an alert if user has not confirmed the email', async () => {
 		render(index, {
@@ -73,9 +73,9 @@ describe('Account information', () => {
 				},
 				quota: { id: 'mail', used: 50, total: 200 }
 			}
-		})
+		});
 		await waitFor(() =>
 			expect(screen.queryByText('Warning: your account is not yet activated')).toBeInTheDocument()
-		)
-	})
-})
+		);
+	});
+});
