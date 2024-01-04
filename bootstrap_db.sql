ca304d1c2b7d867f65b27f2e977a1f3b2532596b
Bruno Thomas <bruno@barreverte.fr>
2024-01-04 10:01:58 +0100

    refactor: dead code (bootstrap db script)

--- bootstrap_db.sql
+++
@@ -1,6 +0,0 @@
-create database iroco;
-create user iroco with encrypted password 'iroco';
-grant all privileges on database iroco to iroco;
-create database test;
-create user test with encrypted password 'test';
-grant all privileges on database test to test;
